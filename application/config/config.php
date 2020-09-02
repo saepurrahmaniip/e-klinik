d9604b146cb678aa547aceea976efd92be73aa65
saepurrahmaniip <boip.rahman@gmail.com>
2020-09-02 15:42:30 +0700

    push config base url

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ date_default_timezone_set("Asia/Jakarta");
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/e-klinik/';
+$config['base_url'] = 'https://www.babagi.id/e-klinik/';
 
 /*
 |--------------------------------------------------------------------------
